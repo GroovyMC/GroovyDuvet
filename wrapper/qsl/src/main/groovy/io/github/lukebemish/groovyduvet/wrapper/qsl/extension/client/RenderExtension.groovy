853793a893cbe84d351b3441f631c948d9268e6f
Luke Bemish <lukebemish@gmail.com>
2022-08-30 18:39:34 -0600

    Figure out sided extensions, actually

--- wrapper/qsl/src/main/groovy/io/github/lukebemish/groovyduvet/wrapper/qsl/extension/client/RenderExtension.groovy
+++ wrapper/qsl/src/main/groovy/io/github/lukebemish/groovyduvet/wrapper/qsl/extension/client/RenderExtension.groovy
@@ -1,10 +1,13 @@
 package io.github.lukebemish.groovyduvet.wrapper.qsl.extension.client
 
+import net.fabricmc.api.EnvType
+import net.fabricmc.api.Environment
 import net.minecraft.client.renderer.RenderType
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.material.Fluid
 import org.quiltmc.qsl.block.extensions.api.client.BlockRenderLayerMap
 
+@Environment(EnvType.CLIENT)
 class RenderExtension {
     static void putAt(RenderType layer, Block block) {
         BlockRenderLayerMap.put(layer, block)
