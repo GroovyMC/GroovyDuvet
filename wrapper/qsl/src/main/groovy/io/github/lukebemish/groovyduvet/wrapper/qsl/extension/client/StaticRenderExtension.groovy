8cd5f6bae187ed9d8d029734a6d9a09cedd4946c
Luke Bemish <lukebemish@gmail.com>
2022-08-30 20:06:01 -0600

    Fix render type extensions

--- wrapper/qsl/src/main/groovy/io/github/lukebemish/groovyduvet/wrapper/qsl/extension/client/StaticRenderExtension.groovy
+++ wrapper/qsl/src/main/groovy/io/github/lukebemish/groovyduvet/wrapper/qsl/extension/client/StaticRenderExtension.groovy
@@ -2,20 +2,19 @@ package io.github.lukebemish.groovyduvet.wrapper.qsl.extension.client
 
 import net.fabricmc.api.EnvType
 import net.fabricmc.api.Environment
+import net.minecraft.client.renderer.ItemBlockRenderTypes
 import net.minecraft.client.renderer.RenderType
 import net.minecraft.world.level.block.Block
 import net.minecraft.world.level.material.Fluid
 import org.quiltmc.qsl.block.extensions.api.client.BlockRenderLayerMap
 
 @Environment(EnvType.CLIENT)
-class RenderExtension {
-    static RenderType leftShift(RenderType layer, Block block) {
+class StaticRenderExtension {
+    static void leftShift(ItemBlockRenderTypes type, RenderType layer, Block block) {
         BlockRenderLayerMap.put(layer, block)
-        return layer
     }
 
-    static RenderType leftShift(RenderType layer, Fluid fluid) {
+    static void leftShift(ItemBlockRenderTypes type, RenderType layer, Fluid fluid) {
         BlockRenderLayerMap.put(layer, fluid)
-        return layer
     }
 }
